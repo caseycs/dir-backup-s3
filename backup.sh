bfe2f5268ccbf088c46925ea46e8303c2818dd8f
Ilia Kondrashov <caseycs@gmail.com>
2018-02-27 06:03:07 +0100

    guess extension

--- backup.sh
+++ backup.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env sh
 set -ex
 
-FILENAME=$(date +%Y-%m-%d_%H-%M-%S)_${NAME}.tar.xz
+case ${COMPRESSOR:-J} in
+a)
+  EXTENSION="lzma"
+  ;;
+j)
+  EXTENSION="bz2"
+  ;;
+J)
+  EXTENSION="xz"
+  ;;
+z)
+  EXTENSION="gz"
+  ;;
+*)
+  echo "Unknown compressor"
+  exit 1
+  ;;
+esac
+
+FILENAME=$(date +%Y-%m-%d_%H-%M-%S)_${NAME}.tar.${EXTENSION}
 
 tar -vc${COMPRESSOR:-J}f $FILENAME -C /backup .
 
-ls -lah *.xz
+ls -lah *.tar*
 
 s3cmd put --verbose --progress $FILENAME s3://${S3_PATH}/
